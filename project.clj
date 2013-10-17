14481b073115f7bbceda44ee7f65ce98ac21281a
Filippo Diotalevi <filippo.diotalevi@gmail.com>
2013-10-17 09:22:55 +0100

    added marginalia and some doc love

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :java-source-paths ["src-java"]
   :main file-kit.core
+  :plugins [[lein-marginalia "0.7.1"]]
   :dependencies
     [[org.clojure/clojure "1.5.1"]
      [commons-io/commons-io "2.4"]])
--- src/file_kit/core.clj
+++ src/file_kit/core.clj
@@ -1,21 +1,17 @@
+;; ## Simple file manipulation in Clojure
+;;
+;; file-kit provides several functions to operate on files.
+;;
+;; It wraps and reuses code from popular libraries like Apache Common IO and 
+;; guava-libraries (everything under the terms of the Apache Softawre License) 
+;; to create the best in breed file utility for Clojure.
+
 (ns file-kit.core
   (:require [clojure.java.io :as io])
   (:import [java.io File])
   (:import [org.apache.commons.io FileUtils]
            [org.clojars.fdiotalevi.guava GuavaFiles])
   (:gen-class))
-
-
-(defn- delete-file-recursively
-  "Delete file f. If it's a directory, recursively delete all its contents.
-Raise an exception if any deletion fails unless silently is true."
-  [f & [silently]]
-  (let [f (io/file f)]
-    (if (.isDirectory f)
-      (doseq [child (.listFiles f)]
-        (delete-file-recursively child silently)))
-    (io/delete-file f silently)))
-
 
 (defmacro defun [name docstring args & body]
   `(do
@@ -24,35 +20,73 @@ Raise an exception if any deletion fails unless silently is true."
      (defmethod ~name String ~args (~name (io/file ~@args)))
      (defmethod ~name :default ~args false)))
 
+;; # Get information about file(s) 
+;;
+;; Use one of the following functions to get information
+;; about one or more files, or one directory
+;;
+;; You can refer to a file writing its path as a string
+;; or using a File object
+
 (defn canonical-path
   "Returns the canonical path of the file or directory."
   [path]
   (.getCanonicalPath (io/file path)))
 
 (defun file?
-  "Returns true if the path is a file; false otherwise."
+  "Returns true if the `path` is a file; false otherwise."
   [path]
   (.isFile ^File path))
 
 (defun directory?
-  "Returns true if the path is a directory; false otherwise."
+  "Returns true if the `path` is a directory; false otherwise."
   [path]
   (.isDirectory ^File path))
 
 (defun exists?
-  "Returns true if path exists; false otherwise."
+  "Returns true if `path` exists; false otherwise."
   [path]
   (.exists ^File path))
 
 (defn size
-  "Returns the size in bytes of a file."
+  "Returns the size in bytes of `file`."
   [file]
   (.length (io/file file)))
+
+(defn cat
+  "Read the content of one or multiple files"
+  [& files]
+  (reduce str (map slurp files)))
+
+(defn lines
+  "Read the content of one or multiple files and split it into lines"
+  [& files]
+  (seq (.split (apply cat files) "\n")))
+
+(defn wc-l
+  "Count the lines of text contained in one or multiple files"
+  [& files]
+  (count (apply lines files)))
 
 (defn ls
   "List files in a directory."
   [dir]
   (seq (.listFiles (io/file dir))))
+
+;; # Modify files
+;;
+;; Following a set of functions to write, modify or remove files
+;; and directory
+
+(defn- delete-file-recursively
+  "Delete file `f`. If it's a directory, recursively delete all its contents.
+Raise an exception if any deletion fails unless silently is true."
+  [f & [silently]]
+  (let [f (io/file f)]
+    (if (.isDirectory f)
+      (doseq [child (.listFiles f)]
+        (delete-file-recursively child silently)))
+    (io/delete-file f silently)))
 
 (defn touch
   "Create a file or update the last modified time."
@@ -98,6 +132,17 @@ Raise an exception if any deletion fails unless silently is true."
   [path]
   (delete-file-recursively path true))
 
+(defn chmod
+  "Change file permissions in a portable way."
+  [path & {:keys [r w x]}]
+  (let [file (io/file path)]
+    (do
+      (if-not (nil? r) (.setReadable file r))
+      (if-not (nil? w) (.setWritable file w))
+      (if-not (nil? x) (.setExecutable file x)))))
+
+;; # Copying and moving files
+
 (defn cp
   "Copy a file, preserving last modified time by default."
   [from to & {:keys [preserve] :or {preserve true}}]
@@ -130,15 +175,10 @@ Raise an exception if any deletion fails unless silently is true."
       :default
         (FileUtils/moveToDirectory from-file to-file true))))
 
-
-(defn chmod
-  "Change file permissions in a portable way."
-  [path & {:keys [r w x]}]
-  (let [file (io/file path)]
-    (do
-      (if-not (nil? r) (.setReadable file r))
-      (if-not (nil? w) (.setWritable file w))
-      (if-not (nil? x) (.setExecutable file x)))))
+;; # Working with temporary files or directory
+;;
+;; Often you'll need to create temporary files or directory 
+;; in your application. The following functions help with that
 
 (defn create-temp-file
   "Create a temporary file with prefix and suffix (extension, like .txt)"
@@ -149,17 +189,5 @@ Raise an exception if any deletion fails unless silently is true."
   "Create a temporary directory"
   []
   (GuavaFiles/createTempDir))
-
-(defn cat
-  [& files]
-  (reduce str (map slurp files)))
-
-(defn lines
-  [& files]
-  (seq (.split (apply cat files) "\n")))
-
-(defn wc-l
-  [& files]
-  (count (apply lines files)))
 
 (def HOME (System/getProperty "user.home"))
